ae50f30bfb72a8871159d87da6b4618bf411ca2a
Jacinda Zhong <jacindaz@gmail.com>
2017-01-05 21:35:21 -0800

    Refactor so that can easily switch between work and personal credentials

--- config/aws_credentials.rb
+++ config/aws_credentials.rb
@@ -1,3 +1,9 @@
-Aws.config.update({
+work_credentials = {
+   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_KEY'])
+}
+
+jacinda_credentials = {
    credentials: Aws::Credentials.new(ENV['JACINDA_AWS_ACCESS_KEY'], ENV['JACINDA_AWS_SECRET_KEY'])
-})
+}
+
+Aws.config.update(work_credentials)
